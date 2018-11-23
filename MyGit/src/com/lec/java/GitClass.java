0ba8832b1d51263058fb6cb64b27c18feaba8112
read0 <read0@DESKTOP-9C44OUJ>
2018-11-23 11:49:57 +0900

    first upload, this is place for memo.

---
+++ MyGit/src/com/lec/java/GitClass.java
@@ -0,0 +1,15 @@
+package com.lec.java;
+
+/**
+ * 주석을 일본어로 달아서 넣어보자!
+ * @author read0
+ *
+ */
+public class GitClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("와우 겨울이다!!!");
+		
+	}
+}
